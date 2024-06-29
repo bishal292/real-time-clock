fe3a07d1fa3d2225bbd5b84157526165b0ea7eb5
bishal292 <bs9107709@gmail.com>
2024-06-29 17:45:21 +0530

    Added digital Clock and functionality to toggle between analog and digital clock

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 console.log(`Script is Running`);
+
+// For toggle button
+let digital = document.querySelectorAll("[data-digital]");
+let analog = document.querySelectorAll("[data-analog]");
+document.getElementById('toggle').addEventListener('change', function() {
+        digital.forEach(element => {
+            element.classList.toggle("none");
+        });
+        analog.forEach(element =>{
+            element.classList.toggle("none");
+        })
+});
+
+
 setInterval(() => {
     currentdate= new Date()
-    hours = currentdate.getHours();
-    if (hours>12){
-        hours -= 12;
+    htime = currentdate.getHours();
+    let hours;
+    if (htime>12){
+        hours -= htime - 12;
+    }else{
+        hours = htime;
     }
     minutetime = currentdate.getMinutes();
     secondstime = currentdate.getSeconds();
@@ -15,4 +32,8 @@ setInterval(() => {
     hour.style.transform = `rotate(${hourrotation}deg)`
     minute.style.transform = `rotate(${minuterotation}deg)`
     seconds.style.transform = `rotate(${secondsrotaion}deg)`
+    
+    document.getElementById('hour-box').innerHTML = htime;
+    document.getElementById('minute-box').innerHTML = minutetime;
+    document.getElementById('seconds-box').innerHTML = secondstime;
 }, 1000);
